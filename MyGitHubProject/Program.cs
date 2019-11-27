58b16c11d9e230e5b8e179d214f0ac1a7be63a12
Richard Charles James Tibang <richardctibang@gmail.com>
2019-11-27 10:46:07 -0500

    test

--- MyGitHubProject/Program.cs
+++ MyGitHubProject/Program.cs
@@ -6,7 +6,12 @@ namespace MyGitHubProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");   
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
         }
     }
 }
