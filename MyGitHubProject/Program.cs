26b32e80f0d75f953009fdaf3cccf7d45c4c6ad7
Richard Charles James Tibang <richardctibang@gmail.com>
2019-11-27 13:33:47 -0500

    aaaa

--- MyGitHubProject/Program.cs
+++ MyGitHubProject/Program.cs
@@ -17,6 +17,11 @@ namespace MyGitHubProject
             {
                 Console.WriteLine("Hello World!");
             }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
         }
     }
 }
